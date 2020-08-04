a94c6c0bad8f436577d438a339cb474a88aa2c9f
nazifall <52135954+nazifall@users.noreply.github.com>
2020-08-04 23:19:55 +0600

    Update Link.java

--- src/video/Link.java
+++ src/video/Link.java
@@ -4,26 +4,23 @@ package video;
 import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
+import java.lang.Thread;
+
 
 public class Link {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 
 		System.setProperty("webdriver.chrome.driver","C:\\sel\\chromedriver.exe");
 		  WebDriver driver = new ChromeDriver();
-		  
-		
-		  driver.manage().window().maximize();
+		  //Maximizes the browser window
+		  driver.manage().window().maximize() ;
 		  driver.get("https://bongobd.com/watch/8NW6fpYBsCY");
-		  Actions action = new Actions(driver);
-		  WebElement elm = driver.findElement(By.xpath("//button[@class='vjs-big-play-button']/span[@class='vjs-icon-placeholder' and 1]"));
-		  action.click(elm).perform();
-      
+		  Thread.sleep(50000); //it will wait 50 seconds because maximum ad limit is 20 seconds
+		  driver.findElement(By.xpath("//*[@id=\"vid1\"]/div[4]/button[1]")).click(); //play button xpath
           
 	}
 }
-
